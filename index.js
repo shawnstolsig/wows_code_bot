018c8958437dc568a3f95368d155856543d5f83e
Shawn Stolsig <shawnstolsig@gmail.com>
2020-09-27 17:18:46 -0700

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+// const { BOT_TOKEN } = require('./config')
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+
+client.login("SuperSecretBotTokenHere");
